3bf7f2b35f26e2dfe83961b0c43f3b7f0ced7f9c
Peter Bryant <petermbryant@gmail.com>
2019-01-24 10:53:56 +0000

    Test Bottom Navigation tab switching

--- test/cocoon_test.dart
+++ test/cocoon_test.dart
@@ -37,4 +37,84 @@ void main() {
     expect(app.theme.accentColor, equals(Colors.black));
     expect(app.theme.brightness, equals(Brightness.light));
   });
+
+  testWidgets('aspect ratio test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Cocoon({
+        "type": "aspect_ratio",
+        "aspect_ratio": 1.7,
+        "child": {
+          "type": "text",
+          "text": "Test",
+        },
+      }),
+    ));
+
+    AspectRatio aspectRatio = tester.widget(find.byType(AspectRatio));
+    expect(aspectRatio.aspectRatio, equals(1.7));
+  });
+
+  testWidgets('bottom navigation test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Cocoon({
+        "type": "bottom_nav_scaffold",
+        "items": [
+          {
+            "icon": "alarm",
+            "title": "Alarms",
+            "body": {
+              "type": "center",
+              "child": {"type": "text", "text": "Alarms Page!"}
+            }
+          },
+          {
+            "icon": "person",
+            "title": "Profile",
+            "body": {
+              "type": "center",
+              "child": {"type": "text", "text": "Profile Page!"}
+            }
+          },
+        ],
+      }),
+    ));
+
+    // The first page should be the Alarms page
+    expect(
+      (tester.widget(find.descendant(
+        of: find.byType(Center),
+        matching: find.text("Alarms Page!"),
+      )) as Text)
+          .data,
+      equals("Alarms Page!"),
+    );
+
+    // Tap the "Profile" tab
+    await tester.tap(find.text("Profile"));
+    await tester.pumpAndSettle();
+
+    // The Profile page should now be visible
+    expect(
+      (tester.widget(find.descendant(
+        of: find.byType(Center),
+        matching: find.text("Profile Page!"),
+      )) as Text)
+          .data,
+      equals("Profile Page!"),
+    );
+
+    // Tap the Alarms tab
+    await tester.tap(find.text("Alarms"));
+    await tester.pumpAndSettle();
+
+    // The Alarms page should now be visible again
+    expect(
+      (tester.widget(find.descendant(
+        of: find.byType(Center),
+        matching: find.text("Alarms Page!"),
+      )) as Text)
+          .data,
+      equals("Alarms Page!"),
+    );
+  });
 }
