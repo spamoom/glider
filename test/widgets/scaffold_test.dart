dfc5c7ba33bfaa218e51109a50e047f846851a11
Peter Bryant <petermbryant@gmail.com>
2019-01-24 15:05:08 +0000

    Test Scaffold widget

---
+++ test/widgets/scaffold_test.dart
@@ -0,0 +1,74 @@
+import 'package:cocoon/cocoon.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('scaffold app bar test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Cocoon({
+        "type": "scaffold",
+        "app_bar": {
+          "type": "app_bar",
+          "title": "App Bar",
+        },
+      }),
+    ));
+
+    AppBar appBar = tester.widget(find.byType(AppBar));
+    Text title = appBar.title;
+    expect(title.data, "App Bar");
+  });
+
+  testWidgets('scaffold fab test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Cocoon({
+        "type": "scaffold",
+        "fab": {
+          "type": "fab",
+          "icon": "alarm",
+          "label": "New Alarm",
+          "destination": {
+            "type": "scaffold",
+          }
+        },
+      }),
+    ));
+
+    FloatingActionButton fab = tester.widget(find.byType(FloatingActionButton));
+    expect(fab.isExtended, isTrue);
+    Icon icon = tester.widget(find.descendant(
+      of: find.byWidget(fab),
+      matching: find.byIcon(Icons.alarm),
+    ));
+    expect(icon.icon, equals(Icons.alarm));
+    Text text = tester.widget(find.descendant(
+      of: find.byWidget(fab),
+      matching: find.text("New Alarm"),
+    ));
+    expect(text.data, equals("New Alarm"));
+  });
+
+  testWidgets('scaffold drawer test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Cocoon({
+        "type": "scaffold",
+        "app_bar": {
+          "type": "app_bar",
+          "title": "App Bar",
+        },
+        "drawer": {
+          "type": "drawer",
+          "child": {
+            "type": "text",
+            "text": "Drawer",
+          }
+        },
+      }),
+    ));
+
+    expect(find.text("Drawer"), findsNothing);
+    await tester.tap(find.byIcon(Icons.menu));
+    await tester.pumpAndSettle();
+    expect(find.text("Drawer"), findsOneWidget);
+  });
+}
