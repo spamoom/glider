723e8e2847f87fbebb820f06a2af9f71e17d69a5
Peter Bryant <petermbryant@gmail.com>
2019-01-24 11:41:52 +0000

    Test Bottom Nav Scaffold

--- test/widgets/bottom_nav_test.dart
+++ test/widgets/bottom_nav_test.dart
@@ -67,4 +67,130 @@ void main() {
       equals("Alarms Page!"),
     );
   });
+
+  testWidgets('bottom navigation scaffold app bar test',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Cocoon({
+        "type": "bottom_nav_scaffold",
+        "app_bar": {
+          "type": "app_bar",
+          "title": "App Bar",
+        },
+        "items": [
+          {
+            "icon": "alarm",
+            "title": "Alarms",
+            "body": {
+              "type": "center",
+              "child": {"type": "text", "text": "Alarms"}
+            }
+          },
+          {
+            "icon": "person",
+            "title": "Profile",
+            "body": {
+              "type": "center",
+              "child": {"type": "text", "text": "Profile"}
+            }
+          }
+        ],
+      }),
+    ));
+
+    AppBar appBar = tester.widget(find.byType(AppBar));
+    Text title = appBar.title;
+    expect(title.data, "App Bar");
+  });
+
+  testWidgets('bottom navigation scaffold fab test',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Cocoon({
+        "type": "bottom_nav_scaffold",
+        "fab": {
+          "type": "fab",
+          "icon": "alarm",
+          "label": "New Alarm",
+          "destination": {
+            "type": "scaffold",
+          }
+        },
+        "items": [
+          {
+            "icon": "alarm",
+            "title": "Alarms",
+            "body": {
+              "type": "center",
+              "child": {"type": "text", "text": "Alarms"}
+            }
+          },
+          {
+            "icon": "person",
+            "title": "Profile",
+            "body": {
+              "type": "center",
+              "child": {"type": "text", "text": "Profile"}
+            }
+          }
+        ]
+      }),
+    ));
+
+    FloatingActionButton fab = tester.widget(find.byType(FloatingActionButton));
+    expect(fab.isExtended, isTrue);
+    Icon icon = tester.widget(find.descendant(
+      of: find.byWidget(fab),
+      matching: find.byIcon(Icons.alarm),
+    ));
+    expect(icon.icon, equals(Icons.alarm));
+    Text text = tester.widget(find.descendant(
+      of: find.byWidget(fab),
+      matching: find.text("New Alarm"),
+    ));
+    expect(text.data, equals("New Alarm"));
+  });
+
+  testWidgets('bottom navigation scaffold drawer test',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Cocoon({
+        "type": "bottom_nav_scaffold",
+        "app_bar": {
+          "type": "app_bar",
+          "title": "App Bar",
+        },
+        "drawer": {
+          "type": "drawer",
+          "child": {
+            "type": "text",
+            "text": "Drawer",
+          }
+        },
+        "items": [
+          {
+            "icon": "alarm",
+            "title": "Alarms",
+            "body": {
+              "type": "center",
+              "child": {"type": "text", "text": "Alarms"}
+            }
+          },
+          {
+            "icon": "person",
+            "title": "Profile",
+            "body": {
+              "type": "center",
+              "child": {"type": "text", "text": "Profile"}
+            }
+          }
+        ]
+      }),
+    ));
+
+    expect(find.text("Drawer"), findsNothing);
+    await tester.tap(find.byIcon(Icons.menu));
+    await tester.pumpAndSettle();
+    expect(find.text("Drawer"), findsOneWidget);
+  });
 }
